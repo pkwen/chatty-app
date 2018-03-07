38b1677b8243af441b39b618e48d568bfa785c31
Danny K Pan <kaiwen006@gmail.com>
2018-03-07 21:44:56 +0000

    server update

--- chatty_server/server.js
+++ chatty_server/server.js
@@ -5,12 +5,29 @@ const SocketServer = require('ws').Server;
 const uuidv4 = require('uuid/v4');
 // Set the port to 3001
 const PORT = 3001;
+var http = require("http")
+
+
+
+var app = express()
+
+app.use(express.static(__dirname + "/"))
+
+var server = http.createServer(app)
+server.listen(PORT)
+
+console.log("http server listening on %d", PORT)
+
+// var wss = new WebSocketServer({ server: server })
+// console.log("websocket server created")
+
+
 
 // Create a new express server
-const server = express()
-  // Make the express server serve static assets (html, javascript, css) from the /public folder
-  .use(express.static('public'))
-  .listen(PORT, '0.0.0.0', 'localhost', () => console.log(`Listening on ${PORT}`));
+// const server = express()
+//   // Make the express server serve static assets (html, javascript, css) from the /public folder
+//   .use(express.static('public'))
+//   .listen(PORT, '0.0.0.0', 'localhost', () => console.log(`Listening on ${PORT}`));
 
 // Create the WebSockets server
 const wss = new SocketServer({ server });
