f1eba210c743f9c270b01793be26b1b70257643d
Danny K Pan <kaiwen006@gmail.com>
2018-03-07 21:53:49 +0000

    server update

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   devtool: 'eval',
   entry: [
     'webpack-dev-server/client?http://localhost:3000',
-    './src/index.jsx'
+    './index.html'
   ],
   output: {
     path: path.join(__dirname, 'dist'),
